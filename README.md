3699d3e3d11a71e2fd1570c819a6a20bfe9f6082
hwzhang92 <hwzhang92@users.noreply.github.com>
2015-09-14 15:46:48 +0800

    增加express，实现获取文件的功能

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 ***
 * excel97-03格式，不支持07以后的格式
 * 用office打开excel文件，如果单元格有感叹号，尽量根据说明修正，否则会出现意想不到的问题
+
+##文件管理RESTful API
+***
+1. GET /files 获取文件列表
+2. GET /files/:id 获取
+3. POST /files 上传文件
+4. POST /files/:id 上传文件，并指定文件名，若存在同名文件，则不做任何操作
+5. PUT /files/:id 更新指定文件名的文件
+6. DELETE /files/:id 删除指定文件名的文件
+
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-node_xj = require("xls-to-json");
+/*node_xj = require("xls-to-json");
   node_xj({
     input: "excel/sample.xls",  // input xls 
     output: null // output json 
@@ -8,4 +8,39 @@ node_xj = require("xls-to-json");
     } else {
       console.log(result);
     }
-  });
+  });*/
+var express = require('express');
+var app = express();
+
+// 获取指定文件
+app.get('/files/:id', function (req, res) {
+  var options = {
+    root: __dirname + '/resources/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+  res.sendFile(req.params.id, options, function(err){
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  })
+});
+
+// 上传文件
+app.post('/files', function(req, res){
+  res.send('files created!');
+})
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('app listening at http://%s:%s', host, port);
+});
