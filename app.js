f39799caa75a28924350094c95093ed6523170a1
hwzhang92 <hwzhang92@users.noreply.github.com>
2015-09-17 15:29:44 +0800

    修改错误处理方式，能够捕获uncaughtException并返回给客户端

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var logger = require('morgan');
 var express = require('express');
 var app = express();
+var domain = require('domain');
 
 var files = require('./routes/files/files');
 var _import = require('./routes/import/import');
 
+app.use(function (req, res, next) {
+    var reqDomain = domain.create();
+    reqDomain.on('error', function (err) { // 下面抛出的异常在这里被捕获
+        res.send(err.status || 500, err.stack); // 成功给用户返回了 500
+    });
+    reqDomain.run(next);
+});
 app.use(logger('dev'));
 
 app.use('/files',files);
@@ -15,12 +23,6 @@ app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
-
-app.use(function(err, req, res, next) {
-    console.log(err);
-    res.status(err.status || 500).end();
-    console.log("yes");
 });
 
 module.exports = app;
